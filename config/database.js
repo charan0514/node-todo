33202b0c9e9dcf0fcf5f766e594afa74064860ee
charan0514 <shree.adiga@gmail.com>
2016-08-04 17:05:56 +0530

    heroku deployment configuartion added

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+	remoteUrl: "mongodb://heroku_3r2kp9t6:f2b8jprque3jmpllllveir1bro@ds139645.mlab.com:39645/heroku_3r2kp9t6",
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
