5100b683ef2185f46b1daf79d712e4cfd66cf777
charan0514 <shree.adiga@gmail.com>
2016-08-08 16:17:53 +0530

    updated db url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
     //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
 	//remoteUrl: "mongodb://heroku_3r2kp9t6:f2b8jprque3jmpllllveir1bro@ds139645.mlab.com:39645/heroku_3r2kp9t6",
-    remoteUrl: "mongodb://charan:admin@ds145415.mlab.com:45415/mean-app-db",
+    //remoteUrl: "mongodb://charan:admin@ds145415.mlab.com:45415/mean-app-db",
+    remoteUrl : "mongodb://heroku_k97c4j34:ecrd42ov67s2u0i35r8rapi3rm@ds145325.mlab.com:45325/heroku_k97c4j34",
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
